8140d3ecb7b0ad16cb2cefd55eeb829e42281ae7
Alexis Ayala <alexisayalaespinoza159@gmail.com>
2026-04-07 11:27:38 -0500

    Regresamos a la normalidad

--- CRM-BLESSING-FRONTEND/src/main.jsx
+++ CRM-BLESSING-FRONTEND/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
-import { ThemeProvider } from './context/ThemeContext'
 import './index.css'
 import App from './App.jsx'
 
@@ -10,9 +9,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,

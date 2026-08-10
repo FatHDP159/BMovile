fb0c6b979b24121f6f6ccac4ac938c3d7006161d
Alexis Ayala <alexisayalaespinoza159@gmail.com>
2026-08-10 10:53:15 -0500

    solucionar bugs

--- CRM-BLESSING-BACKEND/src/app.js
+++ CRM-BLESSING-BACKEND/src/app.js
@@ -28,7 +28,18 @@ require('dotenv').config();
 const app = express();
 
 // --- MIDDLEWARES ---
-app.use(cors({ origin: '*', credentials: true }));
+/* app.use(cors({ origin: '*', credentials: true }));
+app.use(express.json()); */
+
+app.use(cors({
+    origin: 'https://b-mobile-swart.vercel.app',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.options('*', cors());
+
 app.use(express.json());
 
 // --- CONEXIÓN Y CRON ---
